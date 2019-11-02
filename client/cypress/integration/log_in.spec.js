c4aece8211e8872828ea4a6b68b351b8918eeb20
Julian George <jules104@gmail.com>
2019-11-02 15:40:52 +0000

    Add allows user to log in test

---
+++ client/cypress/integration/log_in.spec.js
@@ -0,0 +1,14 @@
+// As a user
+// So I can access my account
+// I can log in
+
+describe('Home page', function() {
+  it('allows user to log in', function() {
+    cy.visit('/');
+    cy.get('.btn-light').click();
+    cy.get('input[name=email]').type(`bigtimecharlie3@gmail.com{enter}`)
+    cy.get('input[name=password]').type(`123456{enter}`)
+    cy.get('.btn-primary').click();
+    cy.url().should('include', '/dashboard')
+  });
+});
