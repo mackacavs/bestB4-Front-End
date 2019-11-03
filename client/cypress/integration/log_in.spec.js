6389215aa63e1fe0b8aa74f39bbf7a3fe9f565cb
Valeria Serci <balerty84@gmail.com>
2019-11-03 14:46:01 +0000

    change button names in tests

--- client/cypress/integration/log_in.spec.js
+++ client/cypress/integration/log_in.spec.js
@@ -5,28 +5,28 @@
 describe('Home page', function() {
   it('allows user to log in', function() {
     cy.visit('/');
-    cy.get('.btn-light').click();
+    cy.get('button[name=login]').click();
     cy.get('input[name=email]').type(`bigtimecharlie3@gmail.com{enter}`)
     cy.get('input[name=password]').type(`123456{enter}`)
-    cy.get('.btn-primary').click();
+    cy.get('button[name=login_submit]').click();
     cy.url().should('include', '/dashboard')
   });
 
   it('stay in the login page with wrong email format', function() {
     cy.visit('/');
-    cy.get('.btn-light').click();
+    cy.get('button[name=login]').click();
     cy.get('input[name=email]').type(`bigtimecharlie3{enter}`)
     cy.get('input[name=password]').type(`123456{enter}`)
-    cy.get('.btn-primary').click();
+    cy.get('button[name=login_submit]').click();
     cy.url().should('include', '/login');
   });
 
   it('stay in the login page with wrong password format', function() {
     cy.visit('/');
-    cy.get('.btn-light').click();
+    cy.get('button[name=login]').click();
     cy.get('input[name=email]').type(`bigtimecharlie3@gmail.com{enter}`)
     cy.get('input[name=password]').type(`12345{enter}`)
-    cy.get('.btn-primary').click();
+    cy.get('button[name=login_submit]').click();
     cy.url().should('include', '/login');
   });
 });
