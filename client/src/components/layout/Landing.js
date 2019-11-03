9a1f330b6c017b2b420727ecc1421c620328cbe0
Valeria Serci <balerty84@gmail.com>
2019-11-03 15:02:12 +0000

    change button names and relative tests

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -13,7 +13,7 @@ const Landing = () => {
           </p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">Sign Up</Link>
-            <Link to="/login" className="btn btn-light">Login</Link>
+            <Link to="/login" name="login" className="btn btn-light">Login</Link>
           </div>
         </div>
       </div>
@@ -22,4 +22,3 @@ const Landing = () => {
 }
 
 export default Landing
-
