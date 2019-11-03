262b08acc19541221a646d43cf80a32b3f5d5b65
Valeria Serci <balerty84@gmail.com>
2019-11-03 14:30:45 +0000

    test for wrong email format

--- client/cypress/integration/log_in.spec.js
+++ client/cypress/integration/log_in.spec.js
@@ -11,4 +11,12 @@ describe('Home page', function() {
     cy.get('.btn-primary').click();
     cy.url().should('include', '/dashboard')
   });
+  it('warns user with wrong email during log in', function() {
+    cy.visit('/');
+    cy.get('.btn-light').click();
+    cy.get('input[name=email]').type(`bigtimecharlie3{enter}`)
+    cy.get('input[name=password]').type(`123456{enter}`)
+    cy.get('.btn-primary').click();
+    cy.url().should('include', '/login');
+  });
 });
