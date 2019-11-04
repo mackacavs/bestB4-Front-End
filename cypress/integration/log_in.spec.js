4236652efa64ad84dbf01133628f8b89af59c4aa
Julian George <jules104@gmail.com>
2019-11-04 18:52:17 +0000

    Moved list item test in to separate file

--- cypress/integration/log_in.spec.js
+++ cypress/integration/log_in.spec.js
@@ -6,7 +6,7 @@ describe('Home page', function() {
   it('allows user to log in', function() {
     cy.visit('/');
     cy.get('a[name=login]').click();
-    cy.get('input[name=email]').type(`bigtimecharlie3@gmail.com{enter}`)
+    cy.get('input[name=email]').type(`happy@gmail.com{enter}`)
     cy.get('input[name=password]').type(`123456{enter}`)
     cy.get('input[name=login_submit][type=submit]').click();
     cy.url().should('include', '/dashboard')
@@ -15,7 +15,7 @@ describe('Home page', function() {
   it('stay in the login page with wrong email format', function() {
     cy.visit('/');
     cy.get('a[name=login]').click();
-    cy.get('input[name=email]').type(`bigtimecharlie3{enter}`)
+    cy.get('input[name=email]').type(`happy{enter}`)
     cy.get('input[name=password]').type(`123456{enter}`)
     cy.get('input[name=login_submit][type=submit]').click();
     cy.url().should('include', '/login');
@@ -24,7 +24,7 @@ describe('Home page', function() {
   it('stay in the login page with wrong password format', function() {
     cy.visit('/');
     cy.get('a[name=login]').click();
-    cy.get('input[name=email]').type(`bigtimecharlie3@gmail.com{enter}`)
+    cy.get('input[name=email]').type(`happy@gmail.com{enter}`)
     cy.get('input[name=password]').type(`12345{enter}`)
     cy.get('input[name=login_submit][type=submit]').click();
     cy.url().should('include', '/login');
