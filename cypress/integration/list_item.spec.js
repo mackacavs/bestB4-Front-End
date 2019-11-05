82544ae3a0bbee034b3d6026d98cf4f7f10759de
Julian George <jules104@gmail.com>
2019-11-05 09:27:03 +0000

    list_item test passsing

--- cypress/integration/list_item.spec.js
+++ cypress/integration/list_item.spec.js
@@ -2,18 +2,18 @@
 // So I can give away food I won't use
 // I can list a food item
 
-// describe('Home page', function() {
-//   it('allows user to cerate a new post', function() {
-//     cy.visit('/');
-//     cy.get('a[name=login]').click();
-//     cy.get('input[name=email]').type(`happy@gmail.com{enter}`)
-//     cy.get('input[name=password]').type(`123456{enter}`)
-//     cy.get('.btn').click();
-//     // cy.get('input[name=login_submit][type=submit]').click();
-//     // cy.get('ul>:nth-child(1)>a').click();
-//     cy.get('.form > [name="description"] ').type('Bacon')
-//     cy.get('.form > [name="expiry"] ').type('25/12/2019')
-//     cy.get('.btn').click();
-//     cy.url().should('include', '/posts')
-//   });
-// });
+describe('Home page', function() {
+  it('allows user to cerate a new post', function() {
+    cy.visit('/');
+    cy.get('a[name=login]').click();
+    cy.get('input[name=email]').type(`happy@gmail.com{enter}`)
+    cy.get('input[name=password]').type(`123456{enter}`)
+    // cy.get('.btn').click();
+    // cy.get('input[name=login_submit][type=submit]').click();
+    // cy.get('ul>:nth-child(1)>a').click();
+    cy.get('.form > [name="description"] ').type('Bacon')
+    cy.get('.form > [name="expiry"] ').type('25/12/2019')
+    cy.get('.flex_container>.post-form>.form>.btn').click();
+    cy.url().should('include', '/profile')
+  });
+});
