deeb47d4bdce83b5237367c6b6ff7e3b6a051cc8
Julian George <jules104@gmail.com>
2019-11-06 10:37:00 +0000

    Setup user messages

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -1,32 +1,72 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 
 const PostItem = ({ auth, post: { _id, description, avatar, name, user, expiry, postcode } }) => {
+
+  const [formData, setFormData] = useState({
+    message: '',
+  });
+
+  const { message } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    setFormData({ ...formData, message: '' })
+  }
+
+
+
   return (
     !auth.loading && auth.user.postcode.slice(0, 2) === postcode.slice(0, 2) && (
       <div className="post bg-white mb">
-        <div>
-          <h4 style={{ width: "150px", marginBottom: "0.5rem" }} className="mb-1">{name}</h4>
-          <img style={{ width: "40px" }}
-            className="round-img"
-            src={avatar}
-            alt="" />
-        </div>
-        <div>
-          <p className="my-1">
-            Description: {description}
-          </p>
-          <p className="my-1">
-            Postcode: {postcode}
-          </p>
+      <div>
+      <h4 style={{ width: "150px", marginBottom: "0.5rem" }} className="mb-1">{name}</h4>
+      <img style={{ width: "40px" }}
+      className="round-img"
+      src={avatar}
+      alt="" />
+      </div>
+      <div>
 
-          <p className="post-date">
-            Expiry Date: {expiry}
-          </p>
+      <p className="my-1">
+      Description: {description}
+      </p>
 
-        </div>
+      <p className="my-1">
+      Postcode: {postcode}
+      </p>
+
+      <p className="post-date">
+      Expiry Date: {expiry}
+      </p>
+
+
+      </div>
+
+
+      <form className="form my-1" onSubmit={e => onSubmit(e)}>
+        <textarea
+          name="message"
+          cols="30"
+          rows="5"
+          placeholder="List an ingredient"
+          value={message}
+          onChange={e => onChange(e)}
+          required
+        ></textarea>
+
+
+        <input type="submit" className="btn btn-dark my-1" value="Submit" />
+      </form>
+
+
+
+
+
       </div>
 
     )
