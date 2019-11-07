26cf2112e7b2396aab66e06307c1a5a4de6bf50e
Julian George <jules104@gmail.com>
2019-11-07 10:28:24 +0000

    Refactored list_item test to use date picker

--- cypress/integration/sign_up.spec.js
+++ cypress/integration/sign_up.spec.js
@@ -8,9 +8,9 @@ describe('Home page', function() {
   it('allows user to sign up', function() {
     cy.visit('/');
     cy.get('.btn-primary').click();
-    cy.get('input[name=name]').type(`Batman{enter}`)
+    cy.get('input[name=name]').type(`UserZ{enter}`)
     cy.get('input[name=postcode]').type(`NW1 2EE{enter}`)
-    cy.get('input[name=email]').type(`batman@gmail.com{enter}`)
+    cy.get('input[name=email]').type(`userz@example.com.com{enter}`)
     cy.get('input[name=password]').type(`123456{enter}`)
     cy.get('input[name=password2]').type(`123456{enter}`)
     cy.get('input.btn.btn-primary').click();
