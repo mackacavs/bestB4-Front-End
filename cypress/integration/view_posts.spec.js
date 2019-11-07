7ada70b3317ba11f2614c82a068caa0096d29c0f
Julian George <jules104@gmail.com>
2019-11-07 09:48:55 +0000

    Refactor view_posts test with test user credentials

--- cypress/integration/view_posts.spec.js
+++ cypress/integration/view_posts.spec.js
@@ -6,7 +6,7 @@ describe('Home page', function() {
   it('allows user to view posts', function() {
     cy.visit('/');
     cy.get('a[name=login]').click();
-    cy.get('input[name=email]').type(`happy@gmail.com{enter}`)
+    cy.get('input[name=email]').type(`usera@example.com{enter}`)
     cy.get('input[name=password]').type(`123456{enter}`)
     cy.get('ul>:nth-child(1)>a').click();
     cy.url().should('include', '/profile')
