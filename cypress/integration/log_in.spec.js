74b83c2e61a6185d0a4466dfff72b275645aa236
Julian George <jules104@gmail.com>
2019-11-07 09:43:49 +0000

    Refactor allow user to log in

--- cypress/integration/log_in.spec.js
+++ cypress/integration/log_in.spec.js
@@ -6,7 +6,7 @@ describe('Home page', function() {
   it('allows user to log in', function() {
     cy.visit('/');
     cy.get('a[name=login]').click();
-    cy.get('input[name=email]').type(`happy@gmail.com{enter}`)
+    cy.get('input[name=email]').type(`usera@example.com{enter}`)
     cy.get('input[name=password]').type(`123456{enter}`)
     // cy.get('input[name=login_submit][type=submit]').click();
     cy.url().should('include', '/profile')
