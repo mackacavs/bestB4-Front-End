edacbc1847a2e5ed3d58617155e31a75e29110ed
Julian George <jules104@gmail.com>
2019-11-07 11:02:30 +0000

    Add contect expector to sign up spec

--- cypress/integration/sign_up.spec.js
+++ cypress/integration/sign_up.spec.js
@@ -6,12 +6,12 @@ describe('Home page', function() {
   it('allows user to sign up', function() {
     cy.visit('/');
     cy.get('.btn-primary').click();
-    cy.get('input[name=name]').type(`Userx{enter}`)
+    cy.get('input[name=name]').type(`UserX{enter}`)
     cy.get('input[name=postcode]').type(`NW1 2EE{enter}`)
     cy.get('input[name=email]').type(`userx@example.com.com{enter}`)
     cy.get('input[name=password]').type(`123456{enter}`)
     cy.get('input[name=password2]').type(`123456{enter}`)
     cy.get('input.btn.btn-primary').click();
-    cy.url().should('include', '/profile')
+    cy.contains("Add some food to share")
   });
 });
