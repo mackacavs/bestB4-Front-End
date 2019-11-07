414266dd4549ec689e86bb9ab3542134581ba23a
Valeria Serci <balerty84@gmail.com>
2019-11-07 22:16:45 +0000

    clean UserPostItem code

--- src/components/posts/UserPostItem.js
+++ src/components/posts/UserPostItem.js
@@ -4,14 +4,15 @@ import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { deleteUserPosts } from '../../actions/post';
 
-const PostItem = ({ deleteUserPosts, auth, post: { _id, description, avatar, name, user, expiry, postcode } }) => {
+const UserPostItem = ({ deleteUserPosts, auth, post: { _id, description, avatar, name, user, expiry, postcode } }) => {
 
   const onSubmit = async (e,_id) => {
-    e.preventDefault()
-    deleteUserPosts(_id)
+    e.preventDefault();
+    deleteUserPosts(_id);
   }
+
   return (
-    !auth.loading && auth.user.postcode.slice(0, 2) === postcode.slice(0, 2) && (
+    !auth.loading && (
       <div className="ingredient bg-white mb">
         <p className="description">
           {description}
@@ -23,9 +24,9 @@ const PostItem = ({ deleteUserPosts, auth, post: { _id, description, avatar, nam
             {expiry}
           </Moment>
         </p>
-        <form onSubmit={e => onSubmit(e,_id)} className="delete-ingredient">
-          <input type="submit" className="btn btn-dark" value="Delete item" />
-        </form>
+        <button onClick={e => onSubmit(e,_id)} className="btn btn-dark delete-ingredient">
+          Delete item
+        </button>
       </div>
     )
   )
@@ -35,4 +36,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps, {deleteUserPosts})(PostItem)
+export default connect(mapStateToProps, {deleteUserPosts})(UserPostItem)
